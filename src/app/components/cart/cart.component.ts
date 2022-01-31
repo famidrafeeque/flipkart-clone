76c54c946aaa55257453e5494e32a83700746ead
famidrafeeque <famidrafeeque313@gmail.com>
2022-01-31 16:09:12 +0530

    A flipkart clone app using angular created

---
+++ src/app/components/cart/cart.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/service/cart.service';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css']
+})
+export class CartComponent implements OnInit {
+
+  public products : any = [];
+  public grantTotal : number = 0;
+  constructor(private cartService: CartService) { }
+
+  ngOnInit(): void {
+    this.cartService.getProducts()
+    .subscribe(res=>{
+      this.products = res;
+      this.grantTotal = this.cartService.getTotalPrice();
+    });
+  }
+  removeItem(item:any){
+    this.cartService.removeCartItem(item);
+  }
+  emptyCart(){
+    this.cartService.removeAllCart();
+  }
+}
